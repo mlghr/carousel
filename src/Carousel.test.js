b576a0ad57abe9a0db2d85ca9da91467c5c5a68e
mlghr <michael.rosenbaum1@gmail.com>
2020-10-21 10:53:57 -0700

    first commit

---
+++ src/Carousel.test.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Card from "./Card";
+import Carousel from "./Carousel";
+
+it("works when you click on the right arrow", function() {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  // expect the first image to show, but not the second
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+});
+
+it("renders without crashing", function() {
+  render(<Card />);
+});
+
+
+it("matches snapshot", function() {
+  const {asFragment} = render(<Card/>);
+  expect(asFragment()).toMatchSnapshot();  
+});
+
+it("renders without crashing", function() {
+  render(<Carousel />);
+});
+
+
+it("matches snapshot", function() {
+  const {asFragment} = render(<Carousel/>);
+  expect(asFragment()).toMatchSnapshot();  
+});
